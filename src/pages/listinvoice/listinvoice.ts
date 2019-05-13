be9b770ea3f95b47c1289f7db89f3930778e67a7
daijichennai <daijiworldtech@gmail.com>
2019-05-13 15:39:54 +0530

    Search issus

--- src/pages/listinvoice/listinvoice.ts
+++ src/pages/listinvoice/listinvoice.ts
@@ -60,14 +60,37 @@ goToInvoiceDetails(invoiceNo){
     });
   }
 
+  onSearch(event) {
+    console.log(event.target.value);
+    var searchTxt = event.target.value;
+    if (searchTxt != '' && searchTxt != null && searchTxt != undefined){
+      this.invoiceJson = this.invoiceJson.filter((item) => {
+        return item.invoice_no.toLowerCase().indexOf(searchTxt.toLowerCase()) > -1;
+      });
+    }else{
+      this.invoiceJson= null;
+      this.storage.get('lsCustCode').then((custCode) => {
+        this.getInvoiceByCustCode(custCode);
+      }); 
+    }
+  }
+
   searchitem(searchbar) {
-    console.log(searchbar);
+    
+    //alert(JSON.stringfy(searchbar));
+
     var val = searchbar.target.value;
     if (val && val.trim() != '') {
       this.invoiceJson = this.invoiceJson.filter((item) => {
         return item.invoice_no.toLowerCase().indexOf(val.toLowerCase()) > -1;
       });
+    }else{
+      
     }
+  }
+
+  ionCancel($event){
+    alert('cancel');
   }
 
 }
