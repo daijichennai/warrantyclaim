6560269130e8505eff75dd1290ddbdd63c9e4077
daijichennai <daijiworldtech@gmail.com>
2019-05-13 12:15:24 +0530

    Claim and Claim Details

--- src/providers/commfunc/commfunc.ts
+++ src/providers/commfunc/commfunc.ts
@@ -7,4 +7,9 @@ export class CommfuncProvider {
   constructor() {
   }
 
+   convertINR(amt) {
+    //return amt.toFixed(2).replace(/(\d)(?=(\d{2})+\d\.)/g, '$1,')
+     return amt.toLocaleString("en-IN", { currency: "INR" })
+  }
+
 }
